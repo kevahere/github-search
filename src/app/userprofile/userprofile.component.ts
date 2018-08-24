cb90df8da25f7e061cbdc302b42f48782720bfe9
kevahere <kevahere@gmail.com>
2018-08-24 17:40:13 +0300

    called api inside the userprofile component failed to display

--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { GitinformationService } from '../gitinformation.service';
+//import { GitinformationService } from '../gitinformation.service';
 import { User } from '../user';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-userprofile',
   templateUrl: './userprofile.component.html',
-  providers:[GitinformationService],
+  //providers: [GitinformationService],
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent implements OnInit {
 
-user:User;
-response:any;
-  constructor(public gitService:GitinformationService){
+  user: User;
+  response: any;
+  constructor(/*public gitService: GitinformationService,*/ private http: HttpClient) {
 
 
   }
 
 
   ngOnInit() {
-    this.gitService.getUserInfo()
-    this.response = this.gitService.getUserInfo()
-    console.log(this.response)
+
+    interface ApiResponse {
+      login: string,
+      avatar_url: string,
+      url: string
+    }
+    this.http.get<ApiResponse>("https://api.github.com/users/kevahere?").subscribe(data =>{
+        this.user = new User(data.login,data.avatar_url,data.url)
+    })
+
+
+
 
   }
 
